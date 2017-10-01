a26d8add6fed8a86cbf7a42d04d3953cc839023d
fabriceedon <fabrice.edon@gmail.com>
2017-10-01 16:17:56 -0400

    remove old ic_launcher, add round dashsurvey icon. Set up all login activity screen and activate login feature

--- app/src/main/java/com/fan/dashsurvey/dashsurvey/MainDashSurvey.java
+++ app/src/main/java/com/fan/dashsurvey/dashsurvey/MainDashSurvey.java
@@ -1,6 +1,8 @@
 package com.fan.dashsurvey.dashsurvey;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,7 +12,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 
+import com.fan.dashsurvey.dashsurvey.authentification.LoginActivity;
 import com.fan.dashsurvey.dashsurvey.notification.DashSurveyNotification;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -94,7 +99,22 @@ public class MainDashSurvey extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_logout) {
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            auth.signOut();
+            // this listener will be called when there is change in firebase user session
+            FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                    if (user == null) {
+                        // user auth state is changed - user is null
+                        // launch login activity
+                        startActivity(new Intent(MainDashSurvey.this, LoginActivity.class));
+                        finish();
+                    }
+                }
+            };
             return true;
         }
 
